64f80755f0a12e7dd5fbe86aed1fbcfa20093ce4
HieuNM <nmhieucoder@gmail.com>
2025-01-12 19:37:49 +0700

    Update baseGeo.service.js

--- app/Services/baseGeo.service.js
+++ app/Services/baseGeo.service.js
@@ -58,7 +58,7 @@ class BaseGeoService {
         try {
             const pathCreateDegrees = changePlacesDegrees(origin) + ';' + changePlacesDegrees(destination);
 
-            const response = await axios.get('http://localhost:5000/route/v1/driving/' + pathCreateDegrees, {
+            const response = await axios.get('http://127.0.0.1:5000/route/v1/driving/' + pathCreateDegrees, {
                 params: {
                     overview: false
                 },
